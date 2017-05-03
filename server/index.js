a99bb43ab5bb403b416a05e62193a044fab35862
Hack Reactor Student <student@hackreactor.com>
2017-05-03 12:34:56 -0700

    parsing results

--- server/index.js
+++ server/index.js
@@ -7,26 +7,29 @@ const request = Promise.promisifyAll(require('request'));
 
 
 const app = express()
+app.port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use('/', express.static(path.join(__dirname + '/../client')));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.status(200).sendFile('/index.html');
 })
 
 
 
 app.get('/search', (req, res) => {
-  console.log(req)
-  let url = 'https://itunes.apple.com/search?term=apple&country=US&entity=podcast'
+  let url = 'https://itunes.apple.com/search?term=apple&country=US&entity=podcast&media=podcast$limit=5'
   request(url, (err, response, body) => {
-    console.log(typeof body);
-    res.status(200).send(body);
+    var data = JSON.parse(body).results
+    console.log(data);
+    res.status(200).send(data);
   })
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(app.port, function () {
+  console.log('Example app listening on port ' + app.port)
 })
+
+module.exports = app;
