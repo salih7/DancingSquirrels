6b83e7f5937ba80d973731c654bd3f35b4b5347b
Hack Reactor Student <student@hackreactor.com>
2017-05-04 16:00:21 -0700

    Complete MVP

--- client/src/components/PodcastListEntry.jsx
+++ client/src/components/PodcastListEntry.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import $ from 'jquery';
 
 class PodcastListEntry extends React.Component {
   constructor(props) {
@@ -12,18 +13,57 @@ class PodcastListEntry extends React.Component {
   }
 
   onClickPodcast() {
-    // post request to the server 
+    // post request to the server
+    if (this.state.renderEpisodes) {
+      this.setState({
+        renderEpisodes: false
+      });
+    } else {
+      $.post('/podcast', {
+        feedUrl: this.props.podcast.feedUrl,
+        collectionId: this.props.podcast.collectionId
+      })
+        .done((episodes) => {
+          // when done renderEpisodes is true AND episodes is set to the results
+          console.log(episodes);
+          this.setState({
+            episodes: episodes,
+            renderEpisodes: true
+          });
+        });
+      }
   }
 
   render() {
     return (
       <div>
-        <div onClick={}><h2>{props.podcast.collectionName}</h2></div>
-        <div><img src={props.podcast.artworkUrl30} /></div>
-        <div>{props.podcast.artistName}</div>
+        <div onClick={this.onClickPodcast}><h2>{this.props.podcast.collectionName}</h2></div>
+        <div><img src={this.props.podcast.artworkUrl100} /></div>
+        <div>{this.props.podcast.artistName}</div>
         {
           this.state.renderEpisodes 
-           ? <div></div>
+           ? <div>
+            {
+              this.state.episodes.map((episode) => {
+              return (
+                <div>
+                  {
+                    episode.episodes.map((ep) => {
+                      return (
+                        <div>
+                          <h3>{ep.title}</h3>
+                          <audio controls>
+                            <source src={ep.url} type="audio/mpeg" />
+                          </audio>
+                        </div>
+                      );
+                    }) 
+                  }
+                </div>
+              );
+            })} 
+
+            </div>
            : null
         }
       </div>
