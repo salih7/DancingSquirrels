0553a502aae46d348bc97c2e3db56f21a089dadf
Hack Reactor Student <student@hackreactor.com>
2017-05-04 16:06:07 -0700

    Match head

---
+++ db/bookshelf.js
@@ -0,0 +1,19 @@
+const bookshelf = require('bookshelf')(knex);
+
+var Users = bookshelf.Model.extend({
+  tableName: 'users'
+})
+
+var Podcasts = bookshelf.Model.extend({
+  tableName: 'podcasts'
+})
+
+var Episodes = bookshelf.Model.extend({
+  tableName: 'episodes'
+})
+
+var user_episodes = bookshelf.Model.extend({
+  tableName: 'user_episodes'
+})
+
+module.exports = bookshelf;
