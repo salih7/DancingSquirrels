5fc0e33ef5ed03b6abb0c21041eb168421778476
Ming <kakacuna@gmail.com>
2017-05-10 19:41:24 -0700

    Refactored server side and User model

--- server/index.js
+++ server/index.js
@@ -4,16 +4,8 @@ const db = require('../db/db.js');
 const routes = require('./routers/routes.js');
 const express = require('express');
 const session = require('express-session');
-const passport = require('passport');
-const FacebookStrategy = require('passport-facebook').Strategy;
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
-const LocalStrategy = require('passport-local').Strategy;
-const UserModel = require('../db/models/User.js');
-const bcrypt = require('bcrypt');
 
-const saltRounds = 10;
-
-const app = express();
+var app = module.exports = express();
 app.port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
@@ -27,97 +19,13 @@ app.use(session({
   cookie: { secure: true }
 }));
 
-
-
 app.use('/', express.static(path.join(__dirname + '/../client')));
 
 app.use('/', routes);
 
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new FacebookStrategy({
-    clientID: 343861912683387,
-    clientSecret: 'b27751166b7231c39db513ae083319d9',
-    callbackURL: `http://localhost:${app.port}/auth/facebook/return`,
-    passReqToCallback: true
-  },
-  function(request, accessToken, refreshToken, profile, cb) {
-    cb(null, profile.id);
-  }
-));
-
-passport.use(new GoogleStrategy({
-  clientID: '151503092115-rhqu4imiqagpc4s30eimt72oi9g41152.apps.googleusercontent.com',
-  clientSecret: 'iqPpvTu14XmKOgZss4ZfHtkA',
-  callbackURL: `http://localhost:${app.port}/auth/google/return`,
-  passReqToCallback: true
-},
-  function(request, accessToken, refreshToken, profile, done) {
-    done(null, profile.id);
-  }
-))
-
-passport.use(new LocalStrategy(
-  (username, password, cb) => {
-    UserModel.User
-    .where( 'username', username )
-    .fetch()
-    .then((user) => {
-      if (!user) {
-        return cb(null, false);
-      } else {
-        let hash = user.attributes.password;
-        bcrypt.compare(password, hash, (err, res) => {
-          if (res) {
-            return cb(null, true);
-          } else {
-            return cb(null, false);
-          }
-        })
-      }
-    })
-  }
-))
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
-app.get('/login/facebook', passport.authenticate('facebook'));
-
-app.get('/login/google', passport.authenticate('google', {
-  scope: [ 'https://www.googleapis.com/auth/plus.login',
-      'https://www.googleapis.com/auth/plus.profile.emails.read' ]
-}));
-
-app.get('/login/local', passport.authenticate('local'));
-
-app.get('/auth/facebook/return',
-  passport.authenticate('facebook', { failureRedirect: '/login/facebook' }),
-  function(req, res) {
-    res.redirect('/#/');
-  });
-
-app.get('/auth/google/return',
-  passport.authenticate('google', { failureRedirect: '/login/google' }),
-  function(req, res) {
-    res.redirect('/#/');
-  });
-
-
-app.post('/login/local', 
-  passport.authenticate('local', { failureRedirect: '/login/local' }),
-  function(req, res) {
-    res.send('validPassword');
-  });
+const config = require('./config.js');
 
 app.listen(app.port, function () {
   console.log('Example app listening on port ' + app.port);
 });
 
-module.exports.app = app;
-module.exports.passport = passport;
--- server/routers/routes.js
+++ server/routers/routes.js
@@ -1,12 +1,6 @@
-const Promise = require('bluebird');
-const request = Promise.promisify(require('request'));
-const parseString = require('xml2js-parser').parseString;
 const express = require('express');
 const UserModel = require('../../db/models/User.js');
-const passport = require('passport');
-const bcrypt = require('bcrypt');
-
-const saltRounds = 10;
+const utils = require('../utils.js');
 
 const router = express.Router();
 
@@ -22,53 +16,24 @@ router.route('/login/local')
 
 router.route('/signup')
   .post((req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-      UserModel.User
-      .forge({ username: username, password: hash })
-      .save()
-      .then((data) => {
-        res.send('success');
-      })
-      .catch((err) => {
+    UserModel.insertOne(req.body.username, req.body.password, (results)=> {
+      if (results.name === 'error') {
         res.send('error');
-      })
+      } else {
+        res.send('success');
+      }
     })
   })
 
 router.route('/search')
   .post((req, res) => {
     let url = `https://itunes.apple.com/search?term=${req.body.search}&country=US&entity=podcast&media=podcast&limit=10`;
-    request(url)
-    .then(function(results) {
-      let data = JSON.parse(results.body).results;
-
-      let tracks = data.map((item) => {
-        var track = {
-          collectionId: item.collectionId,
-          trackId: item.trackId,
-          artistName: item.artistName,
-          collectionName: item.collectionName,
-          releaseDate: item.releaseDate,
-          trackName: item.trackName,
-          collectionViewUrl: item.collectionViewUrl,
-          feedUrl: item.feedUrl,
-          artworkUrl30: item.artworkUrl30,
-          artworkUrl60: item.artworkUrl60,
-          artworkUrl100: item.artworkUrl100,
-          artworkUrl600: item.artworkUrl600,
-          contentAdvisoryRating: item.contentAdvisoryRating,
-          trackCount: item.trackCount,
-          primaryGenreName: item.primaryGenreName,
-          genres: item.genres
-        };
-        return track;
-      });
-      res.status(200).send(tracks);
-    })
-    .catch((err) => {
-      res.status(404).send('We are experiencing some technical difficulties...');
+    utils.grabCollections(url, (err, results) => {
+      if (results) {
+        res.status(200).send(results);
+      } else {
+        res.status(404).send('We are experiencing some technical difficulties...');
+      }
     })
   });
 
@@ -76,38 +41,14 @@ router.route('/podcast')
   .post((req, res) => {
     let url = req.body.feedUrl;
     let collectionId = req.body.collectionId;
-    request(url)
-    .then(function(results) {
-      let xml = results.body;
-      parseString(xml, function(err, result) {
-        let podcasts = result.rss.channel.map((ch) => {
-          let podcast = {
-            collectionId: collectionId,
-            title: ch.title,
-            pubDate: ch.pubDate,
-            summary: ch['itunes:summary'],
-            author: ch['itunes:author'],
-            description: ch.description,
-            image: ch['itunes:image'][0].$.href
-          }
-          podcast.episodes = ch.item.map((obj) => {
-            let track = {
-              collectionId: collectionId,
-              title: obj.title,
-              url: obj.enclosure[0].$.url,
-              duration: obj.duration,
-              pubDate: obj.pubDate
-            };
-            return track;
-          })
-          return podcast;
-        })
-        res.status(200).send(result.rss.channel);
-      });
-    })
-    .catch(function(err) {
-      res.status(404).send('Oopsies...seems we are down!');
+    utils.grabEpisodes(url, collectionId, (err, results) => {
+      if (results) {
+        res.status(200).send(results);
+      } else {
+        res.status(404).send('Oopsies...seems we are down!');
+      }
     });
-  })
+  });
+
 
 module.exports = router;
