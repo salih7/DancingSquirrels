614ea296688b66a02d73752e5d2770606ecf5df1
Ming <kakacuna@gmail.com>
2017-05-10 22:54:00 -0700

    Changed to not upload client ID/Secret

---
+++ server/env/configexample.js
@@ -0,0 +1,14 @@
+//Update Client IDs and Secrets, also change file name to config.js
+
+const FACEBOOK = {
+  ID: 'YOUR_FACEBOOK_ID',
+  SECRET: 'YOUR_FACEBOOK_SECRET'
+}
+
+const FACEBOOK = {
+  ID: 'YOUR_GOOGLE_ID',
+  SECRET: 'YOUR_GOOGLE_SECRET'
+}
+
+module.exports.FACEBOOK = FACEBOOK;
+module.exports.GOOGLE = GOOGLE;
