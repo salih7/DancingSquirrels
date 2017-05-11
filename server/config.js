a91b8ce0492f5e23dbd55d7d62578b049f7366a6
Ming <kakacuna@gmail.com>
2017-05-11 01:47:09 -0700

    removed console.logs

--- server/config.js
+++ server/config.js
@@ -47,7 +47,9 @@ app.get('/auth/facebook/return',
   passport.authenticate('facebook', { failureRedirect: '/#/login/facebook' }),
   function(req, res) {
     req.session.regenerate((err) => {
-      console.log(err);
+      if (err) {
+        console.error(err);
+      }
     })
     res.redirect('/#/');
   });
@@ -56,7 +58,9 @@ app.get('/auth/google/return',
   passport.authenticate('google', { failureRedirect: '/login/google' }),
   function(req, res) {
     req.session.regenerate((err) => {
-      console.log(err)
+      if (err) {
+        console.error(err);
+      }
     })
     res.redirect('/#/');
   });
@@ -65,7 +69,9 @@ app.get('/auth/github/return',
   passport.authenticate('github', { failureRedirect: '/login/github' }),
   function(req, res) {
     req.session.regenerate((err) => {
-      console.log(err)
+      if (err) {
+        console.error(err);
+      }
     })
     res.redirect('/#/');
   });
@@ -74,7 +80,9 @@ app.post('/login/local',
   passport.authenticate('local', { failureRedirect: '/login/local' }),
   function(req, res) {
     req.session.regenerate((err) => {
-      console.log(err)
+      if (err) {
+        console.error(err);
+      }
     })
     res.send('validPassword');
   });
