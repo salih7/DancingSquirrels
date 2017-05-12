4a3f1a88e5b40b6c712b2ae4aa7a8c53b4e1edfd
Steve Kim <emailstevenkim@gmail.com>
2017-05-11 17:01:57 -0700

    Refactor App and PodcastMain

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 import PodcastMain from './components/PodcastMain.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
@@ -7,17 +8,42 @@ import Layout from './layout/Layout.jsx';
 
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
-const app = (
-  <Router>    
-    <div>
-      <Layout />
-      <Switch>
-        <Route exact path="/" component={PodcastMain}/>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-      </Switch>
-    </div>
-  </Router>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      podcasts: [],
+      episodes: []
+    };
 
-ReactDOM.render(app, document.getElementById('podcast-main'));
+    this.onSearch = this.onSearch.bind(this);
+  }
+
+  onSearch(query) {
+    //console.log(query);
+    $.post('/search', { search: query })
+      .done((results) => {
+        console.log(results);
+        this.setState({
+          podcasts: results
+        });
+      });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Layout />
+          <Switch>
+            <Route name="root" exact path="/" component={() => (<PodcastMain onSearch={this.onSearch} podcasts={this.state.podcasts} /> )}/>
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('podcast-main'));
--- client/src/components/PodcastMain.jsx
+++ client/src/components/PodcastMain.jsx
@@ -2,35 +2,41 @@ import React from 'react';
 import $ from 'jquery';
 import Search from './Search.jsx';
 import PodcastList from './PodcastList.jsx';
+import PropTypes from 'prop-types';
 
-class PodcastMain extends React.Component {
-  constructor(props) {
-    super(props);
-    this.search = this.search.bind(this);
-    this.state = {
-      podcasts: []
-    };
-  }
+// class PodcastMain extends React.Component {
+//   constructor(props) {
+//     super(props);
+    // this.search = this.search.bind(this);
+    // this.state = {
+    //   podcasts: []
+    // };
+  // }
 
-  search(query) {
-    //console.log(query);
-    $.post('/search', { search: query })
-      .done((results) => {
-        console.log(results);
-        this.setState({
-          podcasts: results
-        });
-      });
-  }
+  // search(query) {
+  //   //console.log(query);
+  //   $.post('/search', { search: query })
+  //     .done((results) => {
+  //       console.log(results);
+  //       this.setState({
+  //         podcasts: results
+  //       });
+  //     });
+  // }
 
-  render() {
-    return (
-      <div className='main-container'>
-        <Search onSearch={this.search} />
-        <PodcastList podcasts={this.state.podcasts} />
-      </div>
-    );
-  }
-}
+let PodcastMain = function(props) {
+  console.log(props);
+  return (
+    <div className='main-container'>
+      <Search onSearch={props.onSearch} />
+      <PodcastList podcasts={props.podcasts} />
+    </div>
+  );
+};
+
+PodcastMain.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+  podcasts: PropTypes.array.isRequired
+};
 
 export default PodcastMain;
