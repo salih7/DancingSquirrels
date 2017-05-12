67fec9d3055cf8ea9af0cff1b05914ba1e7e07d5
Ming <kakacuna@gmail.com>
2017-05-11 17:45:03 -0700

    Updated User homepage to render

--- client/src/components/UserHomePage.jsx
+++ client/src/components/UserHomePage.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Search from './Search.jsx';
 import PodcastList from './PodcastList.jsx';
-import PropTypes from 'prop-types';
 
-const UserHomePage = function(props) {
-  return (
-    <div>
-      <button>Logout</button>
-      <Search onSearch={props.onSearch} />
-      <PodcastList podcasts={props.podcasts} />
-    </div>
-  );
-};
+
+
+class UserHomePage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='main-container'>
+        <button>Logout</button>
+      </div>
+    );
+  }
+}
+
 
 export default UserHomePage;
