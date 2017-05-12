d16b258f7a1b1ad316f0f75bccf4d3d0b487877c
Salih <salih7@gmail.com>
2017-05-11 23:53:43 -0700

    Test for Heroku deployment

---
+++ server/env/config.js
@@ -0,0 +1,18 @@
+const FACEBOOK = {
+  ID: process.env.FACEBOOK_ID,
+  SECRET: process.env.FACEBOOK_SECRET
+}
+
+const GOOGLE = {
+  ID: process.env.GOOGLE_SECRET,
+  SECRET: process.env.GOOGLE_SECRET
+}
+
+const GITHUB = {
+  ID: process.env.GITHUB_SECRET,
+  SECRET: process.env.GITHUB_SECRET
+}
+
+module.exports.FACEBOOK = FACEBOOK;
+module.exports.GOOGLE = GOOGLE;
+module.exports.GITHUB = GITHUB;
